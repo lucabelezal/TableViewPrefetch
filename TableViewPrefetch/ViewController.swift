a2ea6bec2fcc1a0d44f8fe2886ac5bbacc59dc94
cs-lucas-nascimento <lucabelezal@gmail.com>
2019-10-05 09:56:53 -0300

    Add project

---
+++ TableViewPrefetch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPrefetch
+//
+//  Created by Lucas Nascimento on 05/10/19.
+//  Copyright © 2019 Lucas Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
